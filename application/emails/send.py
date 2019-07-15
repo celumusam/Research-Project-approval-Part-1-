922f2415bc53543333e0f4b088755f42ab495027
christopherchoe <chchchoe@gmail.com>
2019-07-15 12:00:09 -0700

    added formatting to excel sheets

--- application/emails/send.py
+++ application/emails/send.py
@@ -9,6 +9,7 @@ from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email import encoders
 from application.models import database
+from datetime import date
 import json
 import os
 from os import getenv
@@ -29,13 +30,42 @@ def make_message(user, main_workbook):
     name = user.name.replace(' ' , '')
     workbook = xlsxwriter.Workbook(name + '.xlsx')
 
-    # Create sheets for student's workbook and main_workbook (staff) 
+    # Create sheets for student's workbook and main_workbook (staff)
     worksheet = workbook.add_worksheet()
     main_worksheet = main_workbook.add_worksheet(name)
     applied_jobs = user.get_jobs_applied()
-#    applied_jobs = database.userAppliedJobs(user.id)
+    # applied_jobs = database.userAppliedJobs(user.id)
     message = ['{} Weekly Report\n'.format(user.name)]
     message.append('Number Applied this Week: {}\n\n'.format(2))
+
+    main_worksheet.write('A1', 'STUDENT FIRST and LAST NAME')
+    main_worksheet.write('A2', user.name)
+    main_worksheet.write('B1', 'DATE')
+    main_worksheet.write('B2', str(date))
+    main_worksheet.write('C1', 'COHORT')
+
+    worksheet.write('A1', 'STUDENT FIRST and LAST NAME')
+    main_worksheet.write('A2', user.name)
+    worksheet.write('B1', 'DATE')
+    main_worksheet.write('B2', str(date))
+    worksheet.write('C1', 'COHORT')
+
+    main_worksheet.write('A4', 'Date of Application')
+    main_worksheet.write('B4', 'Company Name')
+    main_worksheet.write('C4', 'URL to Job Post')
+    main_worksheet.write('D4', 'Job Title')
+    main_worksheet.write('E4', 'Address')
+    main_worksheet.write('F4', 'Additional Notes')
+    main_worksheet.write('G4', 'Current Status')
+    main_worksheet.write('H4', 'Interview Progress')
+    worksheet.write('A4', 'Date of Application')
+    worksheet.write('B4', 'Company Name')
+    worksheet.write('C4', 'URL to Job Post')
+    worksheet.write('D4', 'Job Title')
+    worksheet.write('E4', 'Address')
+    worksheet.write('F4', 'Additional Notes')
+    worksheet.write('G4', 'Current Status')
+    worksheet.write('H4', 'Interview Progress')
 
     for index, job in enumerate(applied_jobs):
         date = str(job.get('date_applied')).ljust(30)
@@ -43,27 +73,27 @@ def make_message(user, main_workbook):
         title = job.get('job_title').ljust(40)
         message.append(''.join([date, company, title, '\n']))
         # Add to Worksheet
-        row = str(index + 3)
-        worksheet.write('A' + row, job['company'])
-        worksheet.write('B' + row, job['job_title'])
-        worksheet.write('C' + row, job['date_applied'])
-        worksheet.write('D' + row, job['status'])
-        worksheet.write('E' + row, job['url'])
+        row = str(index + 5)
+        worksheet.write('A' + row, job['date_applied'])
+        worksheet.write('B' + row, job['company'])
+        worksheet.write('C' + row, job['url'])
+        worksheet.write('D' + row, job['job_title'])
+        worksheet.write('E' + row, job['location'])
         worksheet.write('F' + row, job['notes'])
-        worksheet.write('G' + row, job['location'])
+        worksheet.write('G' + row, job['status'])
         worksheet.write('H' + row, job['interview_progress'])
 
-        main_worksheet.write('A' + row, job['company'])
-        main_worksheet.write('B' + row, job['job_title'])
-        main_worksheet.write('C' + row, job['date_applied'])
-        main_worksheet.write('D' + row, job['status'])
-        main_worksheet.write('E' + row, job['url'])
+        main_worksheet.write('A' + row, job['date_applied'])
+        main_worksheet.write('B' + row, job['company'])
+        main_worksheet.write('C' + row, job['usl'])
+        main_worksheet.write('D' + row, job['job_title'])
+        main_worksheet.write('E' + row, job['location'])
         main_worksheet.write('F' + row, job['notes'])
-        main_worksheet.write('G' + row, job['location'])
+        main_worksheet.write('G' + row, job['status'])
         main_worksheet.write('H' + row, job['interview_progress'])
-        
+
     workbook.close()
-    
+
     return {
         'name' : user.name,
         'email' : 'jobodysseynotifications@gmail.com', #change later
@@ -98,7 +128,7 @@ def send_email(user_email, email_address, email_pwd, email_body, email_excel):
 def main():
     email_address = getenv('JO_EMAIL')
     email_pwd = getenv('JO_EMAIL_PWD')
-    
+
     # Sets up the workbook for Michelle and staff at Holberton School
     # Each sheet will contain the jobs summary page for each student
     main_workbook_name = 'StudentSummaries' + '.xlsx'
@@ -108,11 +138,11 @@ def main():
 
     users = user_list(main_workbook)
     for user in users:
-#        send_email(user['email'], email_address, email_pwd, user['message'], user['excel'])
+    # send_email(user['email'], email_address, email_pwd, user['message'], user['excel'])
         total_report.append(user['message'])
 
     main_workbook.close()
-    send_email('susan.su.mech@gmail.com', email_address, 
+    send_email('susan.su.mech@gmail.com', email_address,
                email_pwd, '\n\n'.join(total_report), main_workbook_name)
 
 
